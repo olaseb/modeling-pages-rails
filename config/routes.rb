11c05fe85211793b5676137c48b0ce66a5e00595
Sebastien Olascoaga <ola.seb@hotmail.fr>
2016-06-12 12:44:18 +0200

    design part

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'creatives/index'
+
+  get "modeling-pages" => "creatives#index"
+
 end
